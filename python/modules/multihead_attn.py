fixed issue with decoder self attention mask in visualization

python/modules/multihead_attn.py
@@ -60,7 +60,7 @@ class MultiHeadAttention(nn.Module):
 
         # generate attention weights by taking the dot product of queries and keys
         attention_weights = torch.einsum('...thHd,...Thd->...hHtT', q_heads, k_heads) # (N, n_kv_heads, q_heads_per_kv_heads, query_sequence_pad_length, key_value_sequence_pad_length) OR (NhHtT)
-        attention_weights = (1. / math.sqrt(d_queries)) * attention_weights
+        attention_weights = (1.0 / math.sqrt(d_queries)) * attention_weights
 
         if key_padding_mask is not None:
             assert key_padding_mask.shape[0] == attention_weights.shape[0], f"batch dimension for padding is wrong: {key_padding_mask.shape[0]} != {attention_weights.shape[0]}. overall shape: {key_padding_mask.shape} != {attention_weights.shape}"
@@ -71,15 +71,18 @@ class MultiHeadAttention(nn.Module):
             # print(f"key_padding_mask: {key_padding_mask.shape}, attention_weights: {attention_weights.shape}")
 
             # mask away by setting such weights to a large negative number, so that they evaluate to 0 under the softmax
+            # print(f"key_padding_mask: {key_padding_mask}")
             attention_weights = attention_weights.masked_fill(key_padding_mask, -float('inf'))
+            # print(f"attention_weights: {attention_weights == -float('inf')}")
+
+        if self.self_attn and self.in_decoder:
+            assert attn_mask is not None, "attn_mask must be provided for decoder self-attention"
 
         if attn_mask is not None:
             assert attn_mask.shape[0] == attention_weights.shape[3], f"attn_mask length is wrong: {attn_mask.shape[0]} != {attention_weights.shape[3]}. overall shape: {attn_mask.shape} != {attention_weights.shape}"
             assert attn_mask.shape[1] == attention_weights.shape[4], f"attn_mask length is wrong: {attn_mask.shape[1]} != {attention_weights.shape[4]}. overall shape: {attn_mask.shape} != {attention_weights.shape}"
 
             attn_mask = attn_mask.unsqueeze(0).unsqueeze(0).unsqueeze(0)
-
-            # print(f"attn_mask: {attn_mask.shape}, attention_weights: {attention_weights.shape}")
 
             # mask away by setting such weights to a large negative number, so that they evaluate to 0 under the softmax
             attention_weights = attention_weights.masked_fill(attn_mask, -float('inf'))

python/trainers/classic_trainer.py
@@ -292,8 +292,11 @@ class ClassicTrainer():
         target_sequence = self.model.decoder.apply_embedding_transformation(target_sequence) # (N, pad_length, d_model)
         target_sequence = self.model.decoder.apply_positional_embedding(target_sequence) # (N, pad_length, d_model)
 
+        ones = torch.ones(target_sequence.size(1), target_sequence.size(1)).to(target_sequence.device)
+        attn_mask = torch.triu(ones, diagonal=1).bool()
+
         for d, decoder_layer in enumerate(self.model.decoder.decoder_layers):
-            target_sequence, attention_weights = decoder_layer.self_attn(target_sequence, target_sequence, target_sequence, target_sequence_length, tgt_key_padding_mask) # (N, pad_length, d_model)
+            target_sequence, attention_weights = decoder_layer.self_attn(target_sequence, target_sequence, target_sequence, target_sequence_length, tgt_key_padding_mask, attn_mask) # (N, pad_length, d_model)
             
             attention_weights = attention_weights.cpu().detach().contiguous()
 