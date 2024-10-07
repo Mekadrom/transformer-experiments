fixed issue with inconsistent vocab size calculation

score.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     if os.path.exists(os.path.join(run_dir, 'transformer_checkpoint.pth.tar')):
         checkpoint = torch.load(os.path.join(run_dir, 'transformer_checkpoint.pth.tar'))
 
-        model = TranslationTransformerModelProvider().provide_transformer(args, src_bpe_model.vocab_size(), tgt_bpe_model.vocab_size(), tie_embeddings=tgt_bpe_model==src_bpe_model)
+        model = TranslationTransformerModelProvider().provide_transformer(args, utils.vocab_size(src_bpe_model), utils.vocab_size(tgt_bpe_model), tie_embeddings=tgt_bpe_model==src_bpe_model)
 
         model.load_state_dict(checkpoint['model'].state_dict())
 

utils.py
@@ -317,7 +317,7 @@ def beam_search_translate(args, src, tgt_lang_code, model: nn.Module, src_tokeni
         n_completed_hypotheses = min(k, 10)
 
         # Vocab size
-        tgt_vocab_size = tgt_tokenizer.vocab_size()
+        tgt_vocab_size = vocab_size(tgt_tokenizer)
 
         # If the source sequence is a string, convert to a tensor of IDs
         if isinstance(src, str):
@@ -375,9 +375,9 @@ def beam_search_translate(args, src, tgt_lang_code, model: nn.Module, src_tokeni
             prev_word_indices = unrolled_indices // tgt_vocab_size # (k)
             next_word_indices = unrolled_indices % tgt_vocab_size # (k)
 
-            print(f"hypotheses: {hypotheses.shape}")
-            print(f"prev_word_indices: {prev_word_indices.shape}, {prev_word_indices.min()}, {prev_word_indices.max()}")
-            print(f"next_word_indices: {next_word_indices.shape}, {next_word_indices.min()}, {next_word_indices.max()}")
+            # print(f"hypotheses: {hypotheses.shape}")
+            # print(f"prev_word_indices: {prev_word_indices.shape}, {prev_word_indices.min()}, {prev_word_indices.max()}")
+            # print(f"next_word_indices: {next_word_indices.shape}, {next_word_indices.min()}, {next_word_indices.max()}")
 
             # Construct the the new top k hypotheses from these indices
             top_k_hypotheses = torch.cat([hypotheses[prev_word_indices], next_word_indices.unsqueeze(1)], dim=1) # (k, step + 1)