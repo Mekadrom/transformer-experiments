81f67fea7713ea58b4232bc47d2cd644b28e133f
Mekadrom <zadarimm@gmail.com>
2024-12-14 23:30:12 -0500

    added configuration for multiple lang embeddings (slices of one large set of secondary embeddings)

---
+++ modules/per_lang_embedding.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+class PerLangEmbedding(nn.Module):
+    def __init__(self, vocab_size, d_model, per_lang_embedding_layers, embedding_activation):
+        super(PerLangEmbedding, self).__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.per_lang_embedding_layers = per_lang_embedding_layers
+        self.embedding_activation = embedding_activation
+
+        self.initial_embedding = nn.Embedding(vocab_size, d_model)
+        self.embeddings = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(per_lang_embedding_layers)])
+        self.activation = embedding_activation
+
+    def forward(self, sequences):
+        N, P, D = sequences.shape
+
+        lang_indices = sequences[:, 0]
+
+        flat_sequences = self.initial_embedding(sequences).view(-1, D)
+        output = torch.zeros(N*P, self.expert_weights[0].out_features, device=sequences.device)
+
+        for i in range(len(self.embeddings)):
+            embedding_mask = lang_indices == i
+            embedding_input = flat_sequences[embedding_mask.any(dim=1)]
+            embedding_output = self.embeddings[i](embedding_input)
+
+            output[embedding_mask.any(dim=1)] += embedding_output
+
+        return output.view(N, P, -1)
